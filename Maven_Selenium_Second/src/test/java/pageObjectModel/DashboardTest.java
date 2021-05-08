71fd0fd6ac3fdb6a70d2326d03d8d422d864f663
akshay1120 <asjain3031@gmail.com>
2021-05-08 23:27:50 +0530

    Adding new project

---
+++ Maven_Selenium_Second/src/test/java/pageObjectModel/DashboardTest.java
@@ -0,0 +1,38 @@
+package pageObjectModel;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class DashboardTest 
+{
+	WebDriver driver;
+	LoginPage lp = null ;
+	DashboardPage dp = null ;
+	
+	@BeforeMethod
+	public void setUp()
+	{
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("file:///C:/Offline%20Website/index.html");
+		lp = new LoginPage(driver);
+		dp = new DashboardPage(driver);
+	}
+	
+	@Test   //For Tester Working on Dashboard page
+	public void validLoginTest_2()
+	{
+		lp.ValidLogin();
+		Assert.assertEquals(driver.getTitle(),lp.getTitle());
+	}
+	
+	@Test
+	public void coursesHeading()
+	{
+		lp.ValidLogin();
+		dp.getCourses();
+	}
+}
---
+++ Maven_Selenium_Second/src/test/java/pageObjectModel/LoginTest.java
@@ -0,0 +1,31 @@
+package pageObjectModel;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LoginTest 
+{
+	WebDriver driver;
+	LoginPage lp = null ;
+	
+	@BeforeMethod
+	public void setUp()
+	{
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("file:///C:/Offline%20Website/index.html");
+		lp = new LoginPage(driver);
+	}
+	
+	@Test   //For Tester working on Login Page
+	public void validLoginTest_1()
+	{
+		lp.enterUname("kiran@gmail.com");
+		lp.enterPass("123456");
+		lp.clickLogin();
+		Assert.assertEquals(driver.getTitle(),lp.getTitle());
+	}
+}
